f118cf5c6a5597918b716200bbfdd045b1c8f3f0
Begüm Zengin <46397735+begumzengin@users.noreply.github.com>
2022-10-13 14:38:57 +0300

    Create constants.dart

---
+++ parsybot/lib/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const cinnabar = Color(0xffe2474b);
+const pickledBluewood = Color(0xff2f3a56);
+const sanMarino = Color(0xff406d96);
+const sinbad = Color(0xffa8d0da);
+const lightBackground = Color(0xffd8e8e8);
+
+const kDefaultPadding = 20.0;

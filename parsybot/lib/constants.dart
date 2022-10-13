e57abaa6e8ce68349680c23908b16ffd95c76a49
Begüm Zengin <46397735+begumzengin@users.noreply.github.com>
2022-10-13 14:46:05 +0300

    Update constants.dart

--- parsybot/lib/constants.dart
+++ parsybot/lib/constants.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
 
+//light theme
 const cinnabar = Color(0xffe2474b);
 const pickledBluewood = Color(0xff2f3a56);
 const sanMarino = Color(0xff406d96);
 const sinbad = Color(0xffa8d0da);
 const lightBackground = Color(0xffd8e8e8);
+
+//dark theme
+const darkBgChat = Color(0xff414141);
+const darkBgLanding = Color(0xff1a1717);
+const changeLanguage = Color(0xff00568f);
+const landingButtons = Color(0xff98cbff);
+const bubbleColor = Color(0xff192126);
+const darkPrimary = Color(0xff68000f);
+const userBackdrop = Color(0xff00243c);
+const typingBubble = Color(0xff202a32);
+const buttonColor = Color(0xffffb3b0);
 
 const kDefaultPadding = 20.0;

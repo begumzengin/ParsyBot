86247c79bb52be92e8e460a941ad77327da60266
Begüm Zengin <46397735+begumzengin@users.noreply.github.com>
2022-10-13 14:51:28 +0300

    Create landing_page.dart

--- parsybot/lib/main.dart
+++ parsybot/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:parsybot/screens/landing/landing_page.dart';
 
 void main() {
   runApp(MyApp());
---
+++ parsybot/lib/screens/landing/landing_page.dart
@@ -0,0 +1,9 @@
+import 'package:parsybot/constants.dart';
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

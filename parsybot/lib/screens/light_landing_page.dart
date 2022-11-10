80dfbf3c1ae27169fd488b17e20bafca6c6bdc89
Begüm Zengin <46397735+begumzengin@users.noreply.github.com>
2022-11-10 21:44:57 +0300

    Added 'ask me' button to the light landing page

--- parsybot/lib/screens/light_landing_page.dart
+++ parsybot/lib/screens/light_landing_page.dart
@@ -7,8 +7,35 @@ class LandingPage extends StatelessWidget {
     return Scaffold(
         backgroundColor: lightBackground,
         body: Center(
-          child: Image.asset('assets/parsybot_images/parsy_light_landing.png',
-              width: 188.0, height: 185.0),
-        ));
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/parsybot_images/parsy_light_landing.png',
+                width: 188.0, height: 185.0),
+            SizedBox(height: 30),
+            ElevatedButton(
+                onPressed: () {},
+                child: Text('Sor bana!',
+                    style: TextStyle(color: Colors.white, fontSize: 18)),
+                style: ElevatedButton.styleFrom(backgroundColor: cinnabar))
+          ],
+        )));
   }
 }
+
+/*
+Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 500.0),
+                child: Container(
+                    padding: EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: cinnabar,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Center(
+                        child: Text('Sor bana!',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                            )))))
+*/

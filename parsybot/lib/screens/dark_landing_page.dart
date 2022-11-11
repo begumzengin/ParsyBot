17446268576be28739de9bb831447620dcedc822
Begüm Zengin <46397735+begumzengin@users.noreply.github.com>
2022-11-11 12:26:06 +0300

    Added parsy and the 'ask me' button to the dark landing page

--- parsybot/lib/screens/dark_landing_page.dart
+++ parsybot/lib/screens/dark_landing_page.dart
@@ -6,33 +6,58 @@ class DarkLandingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: darkBgLanding,
-        leading: IconButton(
-          onPressed: () {},
-          icon: Icon(Icons.language, color: landingButtons, size: 30.0),
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: darkBgLanding,
+          leading: IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.language, color: landingButtons, size: 30.0),
+          ),
+          actions: <Widget>[
+            Padding(
+                padding: EdgeInsets.only(right: 30.0),
+                child: IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.admin_panel_settings,
+                      color: landingButtons, size: 30.0),
+                )),
+            Padding(
+                padding: EdgeInsets.only(right: 20.0),
+                child: IconButton(
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => LandingPage()));
+                  },
+                  icon:
+                      Icon(Icons.light_mode, color: landingButtons, size: 30.0),
+                )),
+          ],
         ),
-        actions: <Widget>[
-          Padding(
-              padding: EdgeInsets.only(right: 30.0),
-              child: IconButton(
-                onPressed: () {},
-                icon: Icon(Icons.admin_panel_settings,
-                    color: landingButtons, size: 30.0),
-              )),
-          Padding(
-              padding: EdgeInsets.only(right: 20.0),
-              child: IconButton(
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => LandingPage()));
-                },
-                icon: Icon(Icons.light_mode, color: landingButtons, size: 30.0),
-              )),
-        ],
-      ),
-      backgroundColor: darkBgLanding,
-    );
+        backgroundColor: darkBgLanding,
+        body: Center(
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Image.asset('assets/parsybot_images/parsy_dark_landing.png',
+              width: 188.0, height: 185.0),
+          SizedBox(height: 30),
+          SizedBox(
+            width: 204.0,
+            height: 46.0,
+            child: ElevatedButton(
+              onPressed: () {},
+              child: Text(
+                'Sor bana!',
+                style: TextStyle(color: Colors.white, fontSize: 18),
+              ),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(darkPrimary),
+                shadowColor: MaterialStateProperty.all<Color>(userBackdrop),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18.0))),
+              ),
+            ),
+          )
+        ])));
   }
 }

f67ffa51b2104685aed3c110641817870d423359
Begüm Zengin <46397735+begumzengin@users.noreply.github.com>
2022-11-12 15:44:49 +0300

    Create dark_admin_login.dart

---
+++ parsybot/lib/screens/dark_admin_login.dart
@@ -0,0 +1,91 @@
+import 'package:parsybot/constants.dart';
+import 'package:flutter/material.dart';
+
+class AdminLogin extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: darkBgLanding,
+          iconTheme: IconThemeData(color: darkPrimary),
+        ),
+        backgroundColor: darkBgLanding,
+        body: SafeArea(
+            child: Center(
+                child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.admin_panel_settings, size: 70, color: darkPrimary),
+            SizedBox(height: 30),
+            //sistem yöneticisi girişi
+            Text(
+              'Sistem Yöneticisi Girişi',
+              style: TextStyle(
+                color: buttonColor,
+                fontWeight: FontWeight.bold,
+                fontSize: 30,
+              ),
+            ),
+            SizedBox(height: 40),
+
+            //kullanıcı adı
+            Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 350.0),
+                child: Container(
+                    decoration: BoxDecoration(
+                      color: Color(0xFFAAB0B5),
+                      border: Border.all(color: pickledBluewood),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Padding(
+                        padding: const EdgeInsets.only(left: 20.0),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Kullanıcı adı',
+                          ),
+                        )))),
+            SizedBox(height: 20),
+
+            //şifre
+            Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 350.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Color(0xFFAAB0B5),
+                    border: Border.all(color: pickledBluewood),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Padding(
+                      padding: const EdgeInsets.only(left: 20.0),
+                      child: TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'Şifre',
+                        ),
+                      )),
+                )),
+            SizedBox(height: 20),
+
+            //giriş yap butonu
+            SizedBox(
+                width: 167,
+                height: 40,
+                child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text('Giriş yap',
+                        style: TextStyle(color: Colors.white, fontSize: 15)),
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(cinnabar),
+                      shadowColor: MaterialStateProperty.all<Color>(sinbad),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0))),
+                    )))
+          ],
+        ))));
+  }
+}

76395f17423a0bd8ae5468b6330c8146f16d0c7f
Begüm Zengin <46397735+begumzengin@users.noreply.github.com>
2022-11-25 17:33:04 +0300

    Update menuqr_page.dart

--- parsybot/lib/screens/menuqr_page.dart
+++ parsybot/lib/screens/menuqr_page.dart
@@ -5,17 +5,24 @@ class MenuQR extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Menu QR'),
-        backgroundColor: cinnabar,
-      ),
-      body: Image.asset(
-        'assets/parsybot_images/menuqr.jpeg',
-        fit: BoxFit.cover,
-        height: double.infinity,
-        width: double.infinity,
-        alignment: Alignment.center,
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Menu QR'),
+          backgroundColor: cinnabar,
+        ),
+        body: Center(
+            child: InteractiveViewer(
+          boundaryMargin: const EdgeInsets.all(20.0),
+          minScale: 0.1,
+          maxScale: 2,
+          child: Image.asset(
+            'assets/parsybot_images/menuqr.jpeg',
+            /*
+            fit: BoxFit.cover,
+            height: double.infinity,
+            width: double.infinity,
+            */
+            //alignment: Alignment.center,
+          ),
+        )));
   }
 }

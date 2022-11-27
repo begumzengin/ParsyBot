5b290f8b4e907d479325bc489ca8a71019b0f2fa
Begüm Zengin <46397735+begumzengin@users.noreply.github.com>
2022-11-27 12:07:35 +0300

    Create dropdown_language.dart

---
+++ parsybot/lib/dropdown_language.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:parsybot/constants.dart';
+
+const List<String> languageList = <String>['Türkçe', 'English'];
+
+class DropdownLanguage extends StatefulWidget {
+  const DropdownLanguage({super.key});
+
+  @override
+  State<DropdownLanguage> createState() => _DropdownLanguageState();
+}
+
+class _DropdownLanguageState extends State<DropdownLanguage> {
+  String dropdownValue = languageList.first;
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<String>(
+      value: dropdownValue,
+      icon: const Icon(Icons.language),
+      elevation: 16,
+      style: const TextStyle(color: cinnabar),
+      underline: Container(
+        height: 2,
+        color: cinnabar,
+      ),
+      onChanged: (String? value) {
+        setState(() {
+          dropdownValue = value!;
+        });
+      },
+      items: languageList.map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
+    );
+  }
+}

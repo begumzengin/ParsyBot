d7530eb7ee3b43c9188cb2ab7437304212d61bc0
Begüm Zengin <46397735+begumzengin@users.noreply.github.com>
2023-05-12 11:21:18 +0300

    converted main app to stateful

--- parsybot/lib/main.dart
+++ parsybot/lib/main.dart
@@ -17,9 +17,14 @@ void main() async {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     /*
